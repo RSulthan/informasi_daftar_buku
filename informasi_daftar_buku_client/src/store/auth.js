a21fc239e405b568ad5d07954b38f2f89612bf13
SulthanRafif <rsulthan5@gmail.com>
2021-10-08 14:49:35 +0700

    API

--- informasi_daftar_buku_client/src/store/auth.js
+++ informasi_daftar_buku_client/src/store/auth.js
@@ -1,45 +1,54 @@
 import { createStore } from "vuex";
-import axios from "axios";
+// import axios from "axios";
 
 export default createStore({
   state: {
-    authStatus: false,
-    user: null,
-  },
-  mutations: {
-    SET_AUTHENTICATED(state, value) {
-      state.authStatus = value;
-    },
-
-    SET_USER(state, value) {
-      state.user = value;
+    authStatus: true,
+    user: {
+      name: "Rafif",
     },
   },
-  actions: {
-    async login({ dispatch }, credentials) {
-      await axios.get("/sanctum/csrf-cookie");
-      await axios.post("/login", credentials);
+  // mutations: {
+  //   SET_AUTHENTICATED(state, value) {
+  //     state.authStatus = value;
+  //   },
 
-      dispatch("user");
-    },
+  //   SET_USER(state, value) {
+  //     state.user = value;
+  //   },
+  // },
+  // actions: {
+  //   async login({ dispatch }, credentials) {
+  //     await axios.get("/sanctum/csrf-cookie");
+  //     await axios.post("/login", credentials);
 
-    async logout({ commit }) {
-      await axios.post("/logout");
-      commit("SET_AUTHENTICATED", false);
-      commit("SET_USER", null);
-    },
+  //     dispatch("user");
+  //   },
 
-    async user({ commit }) {
-      await axios
-        .get("/api/user")
-        .then((result) => {
-          commit("SET_AUTHENTICATED", true);
-          commit("SET_USER", result.data);
-        })
-        .catch(() => {
-          commit("SET_AUTHENTICATED", false);
-          commit("SET_USER", null);
-        });
-    },
-  },
+  //   async logout({ commit }) {
+  //     await axios.post("/logout");
+  //     commit("SET_AUTHENTICATED", false);
+  //     commit("SET_USER", null);
+  //   },
+
+  //   async user({ commit }) {
+  //     const config = {
+  //       headers: {
+  //         Referer: "http://127.0.0.1:8000",
+  //         "Referrer-Policy": "strict-origin-when-cross-origin",
+  //       },
+  //     };
+
+  //     await axios
+  //       .get("/api/user", config)
+  //       .then((result) => {
+  //         commit("SET_AUTHENTICATED", true);
+  //         commit("SET_USER", result.data);
+  //       })
+  //       .catch(() => {
+  //         commit("SET_AUTHENTICATED", false);
+  //         commit("SET_USER", null);
+  //       });
+  //   },
+  // },
 });
